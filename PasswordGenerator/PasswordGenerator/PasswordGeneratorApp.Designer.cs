90acf8b1bbf35663d21e53d3c0e54ab3373a1177
deanbyrne95 <deanbyrne95@gmail.com>
2016-05-12 16:04:21 +0100

    Uploading Project
    Completed project
    v1.0

---
+++ PasswordGenerator/PasswordGenerator/PasswordGeneratorApp.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace PasswordGenerator
+{
+    partial class PasswordGenerator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PasswordGenerator));
+            this.PasswordGenerationBox = new System.Windows.Forms.TextBox();
+            this.IntroductionLabel = new System.Windows.Forms.Label();
+            this.HowToUseLabel1 = new System.Windows.Forms.Label();
+            this.HowToUseLabel2 = new System.Windows.Forms.Label();
+            this.GeneratePasswordButton = new System.Windows.Forms.Button();
+            this.SavePasswordButton = new System.Windows.Forms.Button();
+            this.MaximumPasswordLengthList = new System.Windows.Forms.ComboBox();
+            this.MaximumPasswordLengthLabel = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.OpenPasswordButton = new System.Windows.Forms.Button();
+            this.CopyTextButton = new System.Windows.Forms.Button();
+            this.SuccessfulLabel = new System.Windows.Forms.Label();
+            this.CheckUpperCase = new System.Windows.Forms.CheckBox();
+            this.CheckLowerCase = new System.Windows.Forms.CheckBox();
+            this.TypeCharacterLabel = new System.Windows.Forms.Label();
+            this.CheckNumbers = new System.Windows.Forms.CheckBox();
+            this.CheckSymbols = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // PasswordGenerationBox
+            // 
+            resources.ApplyResources(this.PasswordGenerationBox, "PasswordGenerationBox");
+            this.PasswordGenerationBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.PasswordGenerationBox.Name = "PasswordGenerationBox";
+            this.PasswordGenerationBox.ReadOnly = true;
+            this.PasswordGenerationBox.TabStop = false;
+            // 
+            // IntroductionLabel
+            // 
+            resources.ApplyResources(this.IntroductionLabel, "IntroductionLabel");
+            this.IntroductionLabel.Name = "IntroductionLabel";
+            // 
+            // HowToUseLabel1
+            // 
+            resources.ApplyResources(this.HowToUseLabel1, "HowToUseLabel1");
+            this.HowToUseLabel1.Name = "HowToUseLabel1";
+            // 
+            // HowToUseLabel2
+            // 
+            resources.ApplyResources(this.HowToUseLabel2, "HowToUseLabel2");
+            this.HowToUseLabel2.Name = "HowToUseLabel2";
+            // 
+            // GeneratePasswordButton
+            // 
+            resources.ApplyResources(this.GeneratePasswordButton, "GeneratePasswordButton");
+            this.GeneratePasswordButton.Name = "GeneratePasswordButton";
+            this.GeneratePasswordButton.UseVisualStyleBackColor = true;
+            this.GeneratePasswordButton.Click += new System.EventHandler(this.GeneratePasswordButton_Click);
+            // 
+            // SavePasswordButton
+            // 
+            resources.ApplyResources(this.SavePasswordButton, "SavePasswordButton");
+            this.SavePasswordButton.Name = "SavePasswordButton";
+            this.SavePasswordButton.UseVisualStyleBackColor = true;
+            this.SavePasswordButton.Click += new System.EventHandler(this.SavePasswordButton_Click);
+            // 
+            // MaximumPasswordLengthList
+            // 
+            this.MaximumPasswordLengthList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.MaximumPasswordLengthList.Items.AddRange(new object[] {
+            resources.GetString("MaximumPasswordLengthList.Items"),
+            resources.GetString("MaximumPasswordLengthList.Items1"),
+            resources.GetString("MaximumPasswordLengthList.Items2"),
+            resources.GetString("MaximumPasswordLengthList.Items3"),
+            resources.GetString("MaximumPasswordLengthList.Items4"),
+            resources.GetString("MaximumPasswordLengthList.Items5"),
+            resources.GetString("MaximumPasswordLengthList.Items6"),
+            resources.GetString("MaximumPasswordLengthList.Items7"),
+            resources.GetString("MaximumPasswordLengthList.Items8"),
+            resources.GetString("MaximumPasswordLengthList.Items9"),
+            resources.GetString("MaximumPasswordLengthList.Items10")});
+            resources.ApplyResources(this.MaximumPasswordLengthList, "MaximumPasswordLengthList");
+            this.MaximumPasswordLengthList.Name = "MaximumPasswordLengthList";
+            // 
+            // MaximumPasswordLengthLabel
+            // 
+            resources.ApplyResources(this.MaximumPasswordLengthLabel, "MaximumPasswordLengthLabel");
+            this.MaximumPasswordLengthLabel.Name = "MaximumPasswordLengthLabel";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "Password File";
+            // 
+            // OpenPasswordButton
+            // 
+            resources.ApplyResources(this.OpenPasswordButton, "OpenPasswordButton");
+            this.OpenPasswordButton.Name = "OpenPasswordButton";
+            this.OpenPasswordButton.UseVisualStyleBackColor = true;
+            this.OpenPasswordButton.Click += new System.EventHandler(this.OpenPasswordButton_Click);
+            // 
+            // CopyTextButton
+            // 
+            resources.ApplyResources(this.CopyTextButton, "CopyTextButton");
+            this.CopyTextButton.Name = "CopyTextButton";
+            this.CopyTextButton.UseVisualStyleBackColor = true;
+            this.CopyTextButton.Click += new System.EventHandler(this.CopyTextButton_Click);
+            // 
+            // SuccessfulLabel
+            // 
+            resources.ApplyResources(this.SuccessfulLabel, "SuccessfulLabel");
+            this.SuccessfulLabel.Name = "SuccessfulLabel";
+            // 
+            // CheckUpperCase
+            // 
+            resources.ApplyResources(this.CheckUpperCase, "CheckUpperCase");
+            this.CheckUpperCase.Checked = true;
+            this.CheckUpperCase.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.CheckUpperCase.Name = "CheckUpperCase";
+            this.CheckUpperCase.UseVisualStyleBackColor = true;
+            // 
+            // CheckLowerCase
+            // 
+            resources.ApplyResources(this.CheckLowerCase, "CheckLowerCase");
+            this.CheckLowerCase.Checked = true;
+            this.CheckLowerCase.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.CheckLowerCase.Name = "CheckLowerCase";
+            this.CheckLowerCase.UseVisualStyleBackColor = true;
+            // 
+            // TypeCharacterLabel
+            // 
+            resources.ApplyResources(this.TypeCharacterLabel, "TypeCharacterLabel");
+            this.TypeCharacterLabel.Name = "TypeCharacterLabel";
+            // 
+            // CheckNumbers
+            // 
+            resources.ApplyResources(this.CheckNumbers, "CheckNumbers");
+            this.CheckNumbers.Checked = true;
+            this.CheckNumbers.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.CheckNumbers.Name = "CheckNumbers";
+            this.CheckNumbers.UseVisualStyleBackColor = true;
+            // 
+            // CheckSymbols
+            // 
+            resources.ApplyResources(this.CheckSymbols, "CheckSymbols");
+            this.CheckSymbols.Checked = true;
+            this.CheckSymbols.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.CheckSymbols.Name = "CheckSymbols";
+            this.CheckSymbols.UseVisualStyleBackColor = true;
+            // 
+            // PasswordGenerator
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.CheckSymbols);
+            this.Controls.Add(this.CheckNumbers);
+            this.Controls.Add(this.TypeCharacterLabel);
+            this.Controls.Add(this.CheckLowerCase);
+            this.Controls.Add(this.CheckUpperCase);
+            this.Controls.Add(this.SuccessfulLabel);
+            this.Controls.Add(this.CopyTextButton);
+            this.Controls.Add(this.OpenPasswordButton);
+            this.Controls.Add(this.MaximumPasswordLengthLabel);
+            this.Controls.Add(this.MaximumPasswordLengthList);
+            this.Controls.Add(this.SavePasswordButton);
+            this.Controls.Add(this.GeneratePasswordButton);
+            this.Controls.Add(this.HowToUseLabel2);
+            this.Controls.Add(this.HowToUseLabel1);
+            this.Controls.Add(this.IntroductionLabel);
+            this.Controls.Add(this.PasswordGenerationBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "PasswordGenerator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox PasswordGenerationBox;
+        private System.Windows.Forms.Label IntroductionLabel;
+        private System.Windows.Forms.Label HowToUseLabel1;
+        private System.Windows.Forms.Label HowToUseLabel2;
+        private System.Windows.Forms.Button GeneratePasswordButton;
+        private System.Windows.Forms.Button SavePasswordButton;
+        private System.Windows.Forms.ComboBox MaximumPasswordLengthList;
+        private System.Windows.Forms.Label MaximumPasswordLengthLabel;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button OpenPasswordButton;
+        private System.Windows.Forms.Button CopyTextButton;
+        private System.Windows.Forms.Label SuccessfulLabel;
+        private System.Windows.Forms.CheckBox CheckUpperCase;
+        private System.Windows.Forms.CheckBox CheckLowerCase;
+        private System.Windows.Forms.Label TypeCharacterLabel;
+        private System.Windows.Forms.CheckBox CheckNumbers;
+        private System.Windows.Forms.CheckBox CheckSymbols;
+    }
+}
+
---
+++ PasswordGenerator/PasswordGenerator/PasswordGeneratorApp.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PasswordGenerator
+{
+    public partial class PasswordGenerator : Form
+    {
+        private readonly string UpperCaseAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private readonly string LowerCaseAlphabet = "abcdefghijklmnopqrstuvwxyz";
+        private readonly string Numbers = "0123456789";
+        private readonly string Symbols = "!@£$%^&*()_-#?/+=~|";
+        private string GeneratedPassword = "";
+        private char[] PossibleChars;
+        private bool UpperCaseEnabled;
+        private bool LowerCaseEnabled;
+        private bool NumbersEnabled;
+        private bool SymbolsEnabled;
+
+        public PasswordGenerator()
+        {
+            InitializeComponent();
+            MaximumPasswordLengthList.SelectedIndex = 0;
+            PasswordGenerationBox.Text = "Generated Password";
+            CheckPossibleChars();
+        }
+
+        private void GetPossibleChars()
+        {
+            //1
+            if (UpperCaseEnabled && LowerCaseEnabled && NumbersEnabled && SymbolsEnabled)
+            {
+                string charChars = UpperCaseAlphabet + LowerCaseAlphabet + Numbers + Symbols;
+                PossibleChars = charChars.ToCharArray();
+            }
+            //2
+            else if (!UpperCaseEnabled && LowerCaseEnabled && NumbersEnabled && SymbolsEnabled)
+            {
+                string charChars = LowerCaseAlphabet + Numbers + Symbols;
+                PossibleChars = charChars.ToCharArray();
+            }
+            //3
+            else if (UpperCaseEnabled && !LowerCaseEnabled && NumbersEnabled && SymbolsEnabled)
+            {
+                string charChars = UpperCaseAlphabet + Numbers + Symbols;
+                PossibleChars = charChars.ToCharArray();
+            }
+            //4
+            else if (UpperCaseEnabled && LowerCaseEnabled && !NumbersEnabled && SymbolsEnabled)
+            {
+                string charChars = UpperCaseAlphabet + LowerCaseAlphabet + Symbols;
+                PossibleChars = charChars.ToCharArray();
+            }
+            //5
+            else if (UpperCaseEnabled && LowerCaseEnabled && NumbersEnabled && !SymbolsEnabled)
+            {
+                string charChars = UpperCaseAlphabet + LowerCaseAlphabet + Numbers;
+                PossibleChars = charChars.ToCharArray();
+            }
+            //6
+            else if (!UpperCaseEnabled && !LowerCaseEnabled && NumbersEnabled && SymbolsEnabled)
+            {
+                string charChars = Numbers + Symbols;
+                PossibleChars = charChars.ToCharArray();
+            }
+            //7
+            else if (!UpperCaseEnabled && LowerCaseEnabled && !NumbersEnabled && SymbolsEnabled)
+            {
+                string charChars = LowerCaseAlphabet + Symbols;
+                PossibleChars = charChars.ToCharArray();
+            }
+            //8
+            else if (!UpperCaseEnabled && LowerCaseEnabled && NumbersEnabled && !SymbolsEnabled)
+            {
+                string charChars = LowerCaseAlphabet + Numbers;
+                PossibleChars = charChars.ToCharArray();
+            }
+            //9
+            else if (UpperCaseEnabled && !LowerCaseEnabled && !NumbersEnabled && SymbolsEnabled)
+            {
+                string charChars = UpperCaseAlphabet + Symbols;
+                PossibleChars = charChars.ToCharArray();
+            }
+            //10
+            else if (UpperCaseEnabled && !LowerCaseEnabled && NumbersEnabled && !SymbolsEnabled)
+            {
+                string charChars = UpperCaseAlphabet + Numbers;
+                PossibleChars = charChars.ToCharArray();
+            }
+            //11
+            else if (UpperCaseEnabled && LowerCaseEnabled && !NumbersEnabled && !SymbolsEnabled)
+            {
+                string charChars = UpperCaseAlphabet + LowerCaseAlphabet;
+                PossibleChars = charChars.ToCharArray();
+            }
+            //12
+            else if (!UpperCaseEnabled && !LowerCaseEnabled && !NumbersEnabled && SymbolsEnabled)
+            {
+                string charChars = Symbols;
+                PossibleChars = charChars.ToCharArray();
+            }
+            //13
+            else if (!UpperCaseEnabled && !LowerCaseEnabled && NumbersEnabled && !SymbolsEnabled)
+            {
+                string charChars = Numbers;
+                PossibleChars = charChars.ToCharArray();
+            }
+            //14
+            else if (!UpperCaseEnabled && LowerCaseEnabled && !NumbersEnabled && !SymbolsEnabled)
+            {
+                string charChars = LowerCaseAlphabet;
+                PossibleChars = charChars.ToCharArray();
+            }
+            //15
+            else if (UpperCaseEnabled && !LowerCaseEnabled && !NumbersEnabled && !SymbolsEnabled)
+            {
+                string charChars = UpperCaseAlphabet;
+                PossibleChars = charChars.ToCharArray();
+            }
+            //16
+            else
+            {
+                string charChars = "";
+                PossibleChars = charChars.ToCharArray();
+            }
+        }
+
+        private void CheckPossibleChars()
+        {
+            //Upper Case
+            if (CheckUpperCase.CheckState == CheckState.Checked)
+            {
+                UpperCaseEnabled = true;
+            }
+            else
+            {
+                UpperCaseEnabled = false;
+            }
+            //Lower Case
+            if (CheckLowerCase.CheckState == CheckState.Checked)
+            {
+                LowerCaseEnabled = true;
+            }
+            else
+            {
+                LowerCaseEnabled = false;
+            }
+            //Numbers
+            if (CheckNumbers.CheckState == CheckState.Checked)
+            {
+                NumbersEnabled = true;
+            }
+            else
+            {
+                NumbersEnabled = false;
+            }
+            //Symbols
+            if (CheckSymbols.CheckState == CheckState.Checked)
+            {
+                SymbolsEnabled = true;
+            }
+            else
+            {
+                SymbolsEnabled = false;
+            }
+
+            GetPossibleChars();
+        }
+
+        private void GeneratePasswordButton_Click(object sender, EventArgs e)
+        {
+            CheckPossibleChars();
+            string s = new string(PossibleChars);
+            Debug.WriteLine(s);
+            Random range = new Random();
+            StringBuilder builder = new StringBuilder();
+            if (PossibleChars.Length > 0)
+            {
+                if (MaximumPasswordLengthList.SelectedIndex >= 0)
+                {
+                    for (int i = 0; i < Int32.Parse(MaximumPasswordLengthList.Text); i++)
+                    {
+                        builder.Append(PossibleChars[range.Next(PossibleChars.Length)]);
+                    }
+                }
+            }
+            else
+            {
+                PasswordGenerationBox.Text = "";
+                SuccessfulLabel.Text = "You must check a set of characters to include in your password!";
+                SuccessfulLabel.ForeColor = Color.Red;
+            }
+            GeneratedPassword = builder.ToString();
+            PasswordGenerationBox.Text = GeneratedPassword;
+            Debug.WriteLine(PasswordGenerationBox.Text);
+        }
+
+        private void SavePasswordButton_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.FileName = DateTime.Now.Year.ToString() + "-" + DateTime.Now.Month.ToString("d2") + "-" + DateTime.Now.Day.ToString("d2") + " - [Site] Password";
+            saveFileDialog1.Filter = "Password Files|*.pass|Text Files|*.txt";
+            saveFileDialog1.Title = "Save your Password";
+
+            if (saveFileDialog1.FileName != "")
+            {
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    using (Stream stream = File.Open(saveFileDialog1.FileName, FileMode.Append, FileAccess.Write))
+                    using (StreamWriter streamWriter = new StreamWriter(stream))
+                    {
+                        streamWriter.Write(PasswordGenerationBox.Text);
+                    }
+                    SuccessfulLabel.Text = "Saved '" + Path.GetFileName(saveFileDialog1.FileName) + "' Successfully!";
+                    SuccessfulLabel.ForeColor = Color.Green;
+                }
+            }
+        }
+
+        private void CopyTextButton_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(PasswordGenerationBox.Text);
+            if (Clipboard.GetText() == PasswordGenerationBox.Text)
+            {
+                SuccessfulLabel.Text = "Successfully Copied Password!";
+                SuccessfulLabel.ForeColor = Color.Red;
+            }
+        }
+
+        private void OpenPasswordButton_Click(object sender, EventArgs e)
+        {
+            Stream stream;
+            openFileDialog1.Filter = "Password Files|*.pass|Text Files|*.txt";
+            openFileDialog1.Title = "Open your Password";
+
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if ((stream = openFileDialog1.OpenFile()) != null)
+                {
+                    string stringFileName = openFileDialog1.FileName;
+                    string fileText = File.ReadAllText(stringFileName);
+                    PasswordGenerationBox.Text = fileText;
+                    if (PasswordGenerationBox.Text == fileText)
+                    {
+                        SuccessfulLabel.Text = "Successfully Opened '" + Path.GetFileName(stringFileName) + "'!";
+                        SuccessfulLabel.ForeColor = Color.Blue;
+                    }
+                }
+            }
+        }
+    }
+}
